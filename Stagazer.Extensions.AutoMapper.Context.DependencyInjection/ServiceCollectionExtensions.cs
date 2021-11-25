ae9b81afdefcdfdd522bca630d19376a609fe6ea
stargazer-w <330589465@qq.com>
2021-11-25 11:18:34 +0800

    使用第三方库实现装饰器效果

--- Stagazer.Extensions.AutoMapper.Context.DependencyInjection/ServiceCollectionExtensions.cs
+++ Stagazer.Extensions.AutoMapper.Context.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using AutoMapper;
+﻿using AutoMapper;
+using MassiveDynamicProxyGenerator.Microsoft.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Stagazer.Extensions.AutoMapper.Context.DependencyInjection
 {
@@ -12,49 +8,7 @@ namespace Stagazer.Extensions.AutoMapper.Context.DependencyInjection
     {
         public static void AddAutoMapperContext<T>(this IServiceCollection services) where T : notnull
         {
-            ReplaceMapper<T>(services);
-        }
-
-        private static void ReplaceMapper<T>(IServiceCollection services) where T : notnull
-        {
-            var sd = services.First(x => x.ServiceType == typeof(IMapper));
-
-            switch(sd)
-            {
-                case { ImplementationType: { } t }:
-                    {
-                        services.Add(new ServiceDescriptor(t, t, sd.Lifetime));
-                        services.Replace(ServiceDescriptor.Scoped(typeof(IMapper), sp =>
-                        {
-                            var bkMapper = sp.GetRequiredService(t);
-                            var context = sp.GetRequiredService<T>();
-                            return new ContextMapper<T>((IMapper)bkMapper, context);
-                        }));
-                    }
-                    break;
-                case { ImplementationInstance: { } o }:
-                    {
-                        var t = o.GetType();
-                        services.Add(new ServiceDescriptor(t, o));
-                        services.Replace(ServiceDescriptor.Scoped(typeof(IMapper), sp =>
-                        {
-                            var bkMapper = sp.GetRequiredService(t);
-                            var context = sp.GetRequiredService<T>();
-                            return new ContextMapper<T>((IMapper)bkMapper, context);
-                        }));
-                    }
-                    break;
-                case { ImplementationFactory: { } f }:
-                    {
-                        services.Replace(ServiceDescriptor.Scoped(typeof(IMapper), sp =>
-                        {
-                            var bkMapper = f(sp);
-                            var context = sp.GetRequiredService<T>();
-                            return new ContextMapper<T>((IMapper)bkMapper, context);
-                        }));
-                    }
-                    break;
-            }
+            services.AddDecorator<IMapper, ContextMapper<T>>();
         }
     }
 }
